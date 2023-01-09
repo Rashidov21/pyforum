37373b5a61483862a0d5b692cd36a65d9d9c9f51
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-09 16:15:12 +0500

    update lesson

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 from .models import Category, Tag, Topic, Comment
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Topic
+    paginate_by = 10
+    context_object_name = "topics"
     template_name = "index.html"
 
 
